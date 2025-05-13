546934f2ae3846fe3d13ff8d5322fee8a03338c8
Truman <trumanjfchan@gmail.com>
2025-05-12 22:48:40 -0700

    feat: make a challenge

--- server.js
+++ server.js
@@ -27,6 +27,40 @@ db.query(createUsersTableSQL, (err) => {
     }
 });
 
+const createChallengesTableSQL = `
+  CREATE TABLE IF NOT EXISTS challenges (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(20) NOT NULL,
+    activity VARCHAR(20) NOT NULL
+  );
+`;
+db.query(createChallengesTableSQL, (err) => {
+    if (err) {
+        console.error('Error creating challenges table:', err);
+    } else {
+        console.log('Challenges table confirmed.');
+    }
+});
+
+const createChallengeUserTableSQL = `
+  CREATE TABLE IF NOT EXISTS challenge_user (
+    user_id INT NOT NULL,
+    challenge_id INT NOT NULL,
+    role ENUM('poster', 'opponent') NOT NULL,
+    PRIMARY KEY (user_id, challenge_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (challenge_id) REFERENCES challenges(id) ON DELETE CASCADE
+  );
+`;
+db.query(createChallengeUserTableSQL, (err) => {
+    if (err) {
+        console.error('Error creating challenge_user table:', err);
+    } else {
+        console.log('Challenge_user table confirmed.');
+    }
+});
+
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -58,6 +92,46 @@ io.on('connection', (socket) => {
             }
         });
     });
+
+    socket.on('challenge', (data) => {
+        db.query(`INSERT INTO challenges (title, activity) VALUES (?, ?)`, [data.title, data.challenge], (err, results) => {
+            if (err) {
+                console.error('Error inserting challenges:', err);
+                return
+            } else {
+                console.log('Added challenge!');
+
+                const challengeId = results.insertId;
+                db.query(`SELECT * FROM users WHERE name = ?`, [data.poster], (err, results) => {
+                    const poster = results[0].id;
+                    console.log(poster);
+
+                    db.query(`SELECT * FROM users WHERE name = ?`, [data.opponent], (err, results) => {
+                        const opponent = results[0].id;
+                        console.log(opponent)
+
+                        db.query(`INSERT INTO challenge_user (user_id, challenge_id, role) VALUES (?, ?, 'poster');`, [poster, challengeId], (err, results) => {
+                            if (err) {
+                                console.error('Error inserting poster_challenge:', err);
+                                return
+                            } else {
+                                console.log('Added poster_challenge!');
+
+                                db.query(`INSERT INTO challenge_user (user_id, challenge_id, role) VALUES (?, ?, 'opponent');`, [opponent, challengeId], (err, results) => {
+                                    if (err) {
+                                        console.error('Error inserting opponent_challenge:', err);
+                                        return
+                                    } else {
+                                        console.log('Added opponent_challenge!');
+                                    }
+                                });
+                            }
+                        });
+                    });
+                });
+            }
+        });
+    })
 
     socket.on('disconnect', () => {
         console.log('user disconnected: ' + socket.id);
